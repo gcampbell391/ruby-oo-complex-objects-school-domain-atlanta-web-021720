bdff755a1951e9ca91cbce5cb3717f0e54cf7863
gcampbell391 <gcampbell391@gmail.com>
2020-02-19 19:27:59 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+    attr_accessor :name, :roster 
+    def initialize(name, roster = {})
+        @name = name 
+        @roster = roster
+    end 
+    def add_student(name, grade)
+        repeated_grade = roster.has_key? grade 
+        if repeated_grade == true 
+        roster[grade] << name
+        else 
+            roster[grade] = []
+            roster[grade] << name
+        end 
+        roster
+    end 
+    def grade(grade)
+        roster[grade]
+    end 
+    def sort 
+        @roster.each do |grade, name|
+        @roster[grade] = name.sort
+        end 
+    end 
+end 
